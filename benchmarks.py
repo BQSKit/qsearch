d21f43d8e7648e66a00cd9d0090cc9ac99d71264
Marc Davis <marc.davis@berkeley.edu>
2019-08-30 12:31:37 -0700

    did some basic cleanup for preparing for release

--- benchmarks.py
+++ benchmarks.py
@@ -1,11 +1,7 @@
-import numpy as np
-
 import search_compiler as sc
 from search_compiler import gates, advanced_gates
 
-#from qasm_parser import parse_qasm
-
-project = sc.Project("extra-debugging")
+project = sc.Project("benchmarks")
 project.add_compilation("qft2", gates.qft(4))
 project.add_compilation("qft3", gates.qft(8))
 project.add_compilation("fredkin", gates.fredkin)
@@ -16,14 +12,14 @@ project.add_compilation("logical or", gates.logical_or)
 project.add_compilation("miro", advanced_gates.mirogate)
 project.add_compilation("hhl", advanced_gates.HHL)
 
-# 4 qubit stuff here
+# 4 qubit benchmarks (WARNING: These may take days to run.)
 #project.add_compilation("qft4", gates.qft(16))
 #project.add_compilation("full adder", gates.full_adder)
 #project.add_compilation("ethelyne", advanced_gates.ethelyne)
 
 
-# compiler configuration here
-project["beams"] = -1
+# compiler configuration
+project["beams"] = -1 # tell the compiler to use all available processors by using beams (this is usually a speedup, but not always)
 
-#project.run()
+project.run()
 
