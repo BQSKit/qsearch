1ea35133211b47c242eedb63405e1d2cc5390ec4
Ethan Smith <ethan@ethanhs.me>
2020-03-03 23:17:49 -0800

    Use Python 3 for build script

--- scripts/build_rust.sh
+++ scripts/build_rust.sh
@@ -1,1 +1,1 @@
-cd native &&  RUSTFLAGS="-C target-cpu=native" maturin build --release -i python --manylinux 2010-unchecked && pip install target/wheels/scrs-*.whl -U && cd ..
+cd native &&  RUSTFLAGS="-C target-cpu=native" maturin build --release -i python3 --manylinux 2010-unchecked && pip install target/wheels/scrs-*.whl -U && cd ..
