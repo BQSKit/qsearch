bb2cbe43374fd69192c7e4ad14737759c5b88c6c
Marc Davis <marc.davis@berkeley.edu>
2020-04-09 23:04:04 -0700

    fixed roundtrip.py

--- scripts/roundtrip.py
+++ scripts/roundtrip.py
@@ -18,23 +18,23 @@
 
 import search_compiler as sc
 import qiskit
-from qiskit import QuantumCircuit
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
 import numpy as np
-from search_compiler import gates, advanced_gates, utils, gatesets
+from search_compiler import unitaries, advanced_unitaries, utils, gatesets
 
 # create a new project
 project = sc.Project("round-trip")
 
 # add some gates to compile
-project.add_compilation("qft2", gates.qft(4))
-project.add_compilation("qft3", gates.qft(8))
-project.add_compilation("fredkin", gates.fredkin)
-project.add_compilation("toffoli", gates.toffoli)
-project.add_compilation("peres", gates.peres)
-project.add_compilation("logical or", gates.logical_or)
+project.add_compilation("qft2", unitaries.qft(4))
+project.add_compilation("qft3", unitaries.qft(8))
+project.add_compilation("fredkin", unitaries.fredkin)
+project.add_compilation("toffoli", unitaries.toffoli)
+project.add_compilation("peres", unitaries.peres)
+project.add_compilation("logical or", unitaries.logical_or)
 
-project.add_compilation("miro", advanced_gates.mirogate)
-project.add_compilation("hhl", advanced_gates.HHL)
+project.add_compilation("miro", advanced_unitaries.mirogate)
+project.add_compilation("hhl", advanced_unitaries.HHL)
 
 # 4 qubit benchmarks (WARNING: These may take days to run.)
 #project.add_compilation("qft4", gates.qft(16))
@@ -47,12 +47,12 @@ project.run()
 # prepare a Qiskit backend to generate unitaries
 backend = qiskit.BasicAer.get_backend('unitary_simulator')
 
-for product in project.compilations():
+for compilation in project.compilations():
     # get the original target unitary and final 
-    U1 = project.get_cdict(product)
+    U1 = project.get_target(compilation)
 
     # generate and run Qiskit code to create a Qiskit version of the circuit
-    qiskit_code = sc.assembler.assemble(c, v, sc.assembler.ASSEMBLY_QISKIT) 
+    qiskit_code = project.assemble(compilation, sc.assembler.ASSEMBLY_QISKIT)
     exec(qiskit_code)
 
     # generate a unitary from the Qiskit circuit
@@ -61,5 +61,5 @@ for product in project.compilations():
     U2 = sc.utils.endian_reverse(U2) # switch from Qiskit endianess search_compiler endianess
 
     # Compare the two unitaries and print the result.  The values should be close to 0.
-    print("Match for {}: {}".format(product, sc.utils.matrix_distance_squared(U1, U2)))
+    print("Match for {}: {}".format(compilation, sc.utils.matrix_distance_squared(U1, U2)))
 
