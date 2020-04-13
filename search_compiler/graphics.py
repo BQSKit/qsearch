48920cf870af5fde192db6547563a6543fd898f5
Marc Davis <marc.davis@berkeley.edu>
2020-04-13 00:33:01 -0700

    properly included the license for the code we borrowed

--- search_compiler/graphics.py
+++ search_compiler/graphics.py
@@ -1,5 +1,35 @@
-import sys
+# This is a modified version of code written by Rick Muller "Simplified Plotting Routines for Quantum Circuits"
+# https://github.com/rpmuller/PlotQCircuit/blob/master/PlotQCircuit.ipynb
+# The License of Rick Muller's code is reproduced here:
+"""
+BSD 2-Clause License
 
+Copyright (c) 2017, Rick Muller
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+import sys
 import numpy as np
 
 try:
@@ -10,8 +40,6 @@ except ImportError:
     HAVE_MATPLOTLIB = False
 
 
-# This code was written by Rick Muller "Simplified Plotting Routines for Quantum Circuits"
-# https://github.com/rpmuller/PlotQCircuit/blob/master/PlotQCircuit.ipynb
 
 def plot_quantum_circuit(gates,inits={},labels=[],plot_labels=True,**kwargs):
     """Use Matplotlib to plot a quantum circuit.
