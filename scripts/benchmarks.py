e8ac490f7bebd5865e980f463bb173edde83b5aa
Marc Davis <marc.davis@berkeley.edu>
2020-04-29 17:06:54 -0700

    made a separate script for average timing

--- scripts/benchmarks.py
+++ scripts/benchmarks.py
@@ -1,7 +1,11 @@
 import search_compiler as sc
 from search_compiler import unitaries, advanced_unitaries
 import time
+
+# create the project
 project = sc.Project("benchmarks")
+
+# add a unitaries to compile
 project.add_compilation("qft2", unitaries.qft(4))
 project.add_compilation("qft3", unitaries.qft(8))
 project.add_compilation("fredkin", unitaries.fredkin)
@@ -20,18 +24,18 @@ project.add_compilation("hhl", advanced_unitaries.HHL)
 
 # compiler configuration example
 #project["gateset"] = sc.gatesets.QubitCNOTRing() # use this to synthesize for the ring topology instead of the default line topology
-project["solver"]  = sc.solver.BFGS_Jac_SolverNative() # use this to force the compiler to use the Rust version of the BFGS solver instead of using the default setting
+#project["solver"]  = sc.solver.BFGS_Jac_SolverNative() # use this to force the compiler to use the Rust version of the BFGS solver instead of using the default setting
+#project["verbosity"] = 2 # use this to have more information reported to stdout and the log files, or set it to 0 to disable logging altogether
 
-times = {}
-for compilation in project.compilations:
-    times[compilation] = 0
+# once everything is set up, let the project run!
+project.run()
 
-for _ in range(10):
-    project.reset()
-    project.run()
-    for compilation in project.compilations:
-        times[compilation] += project.get_time(compilation)
 
-for compilation in project.compilations:
-    print(f'Compilation {compilation} took {times[compilation]/10}s on average.')
+# once its done you can use the following functions to get output
+# compilation_names = project.compilations # returns a list of the names that were specified when adding unitaries
+# circuit, vector = project.get_result(compilation_names[0]) # get a circuit/vector combination, which is the search_compiler format for describing finished circuits
+# project.assemble(compilation_names[0], write_location="my_circuit.qasm") # export the circuit as openqasm
+# project.assemble(compilation_names[0], language=assembly.ASSEMBLY_QISKIT, write_location="my_circuit.py") # export the circuit as a qiskit script
+
+# Read the wiki for more information and more features: https://github.com/WolfLink/search_compiler/wiki
 
