05ca146031a01fe4a4cb108223dd943a0107afac
Marc Davis <marc.davis@berkeley.edu>
2020-06-12 19:27:49 -0700

    changed how the options class works

--- qsearch/options.py
+++ qsearch/options.py
@@ -4,9 +4,8 @@ A class for holding and managing options that are passed to various other classe
 
 
 class Options():
-    def __init__(self, **args):
-        self.dict = args
-        self.defaults = {}
+    def __init__(self, **defaults):
+        self.defaults = defaults
         self.smart_defaults = {}
 
     def filtered(self, *names):
@@ -19,34 +18,26 @@ class Options():
         newOptions.smart_defaults = new_smart_defaults
         return newOptions
 
+    def __getattr__(self, name):
+        if name in self.smart_defaults:
+            return self.smart_defaults[name](self)
+        elif name in self.defaults:
+            return self.defaults[name]
+        raise AttributeError()
+
     # creates an Options object with the same defaults but without any specific values
     def empty_copy():
-        newOptions = Options()
-        newOptions.defaults = self.defaults
+        newOptions = Options(**self.defaults)
         newOptions.smart_defaults = self.smart_defaults
 
-    def __setitem__(self, key, value):
-        self.dict[key] = value
-
-    def __getitem__(self, key):
-        if key in self.dict:
-            return self.dict[key]
-        elif key in smart_defaults:
-            return self.smart_defaults[key](self)
-        else:
-            return self.defaults[key]
-
-    def __delitem__(self, key):
-        del self.dict[key]
-
     # the options class will fallback to default values if a normal value is not specified
-    def set_default(self, **args):
+    def set_defaults(self, **args):
         for key in args:
             self.defaults[key] = args[key]
 
     # if a smart_default function is specified, it will be called instead of falling back to normal defaults
     # smart default functions should take an Options class and return a value
-    def set_smart_default(self, **args):
+    def set_smart_defaults(self, **args):
         for key in args:
             self.defaults[key] = args[key]
 
