bd97cba76ec90372e7c4ac1e6a7d956152f90553
Ethan Smith <ethan@ethanhs.me>
2020-06-20 22:38:13 -0700

    Add gateset tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,16 @@
 """A pytest fixture to create a new qsearch project in a temporary directory"""
 import pytest
 from qsearch import Project
+from qsearch.utils import matrix_distance_squared
 
 @pytest.fixture
 def project(tmp_path):
     return Project(str(tmp_path))
+
+@pytest.fixture
+def check_project():
+    def check(project):
+        for comp in project.compilations:
+            c, v = project.get_result(comp)
+            assert matrix_distance_squared(c.matrix(v), project.get_target(comp)) < 1e-10
+    return check
---
+++ tests/test_gatesets.py
@@ -0,0 +1,31 @@
+from qsearch import gatesets, unitaries, advanced_unitaries
+
+def test_qubit_cnot_linear(project, check_project):
+    project['gateset'] = gatesets.QubitCNOTLinear()
+    project.add_compilation('fredkin', unitaries.fredkin)
+    project.run()
+    check_project(project)
+
+def test_zxzxzcnotlinear(project, check_project):
+    project['gateset'] = gatesets.ZXZXZCNOTLinear()
+    project.add_compilation('qft2', unitaries.qft(4))
+    project.run()
+    check_project(project)
+
+def test_qiskitu3linear(project, check_project):
+    project['gateset'] = gatesets.QiskitU3Linear()
+    project.add_compilation('hhl', advanced_unitaries.HHL)
+    project.run()
+    check_project(project)
+
+def test_cnotring(project, check_project):
+    project['gateset'] = gatesets.QubitCNOTRing()
+    project.add_compilation('qft2', unitaries.qft(4))
+    project.run()
+    check_project(project)
+
+def test_adjacency_list(project, check_project):
+    project['gateset'] = gatesets.QubitCNOTAdjacencyList([(0,1), (1,2), (2,3), (2,0)])
+    project.add_compilation('qft2', unitaries.qft(4))
+    project.run()
+    check_project(project)
