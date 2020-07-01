fd8dab1e42a73a3ba41c4446948e2c0d94fc22df
Marc Davis <marc.davis@berkeley.edu>
2020-06-30 23:08:29 -0700

    added more qutrit test cases.  will clean this up later

--- examples/qutrit_csum.py
+++ examples/qutrit_csum.py
@@ -5,5 +5,8 @@ p = qsearch.Project("qutrit-csum")
 p["gateset"] = gatesets.QutritCPIPhaseLinear()
 p["verbosity"] = 2
 
-p.add_compilation("csum", circuits.CSUMStep().matrix([]))
+p.add_compilation("cob-csum", circuits.CSUMStep().matrix([]), solver=solver.COBYLA_Solver())
+p.add_compilation("jac-csum", circuits.CSUMStep().matrix([]), solver=solver.BFGS_Jac_Solver())
+p.add_compilation("lss-csum", circuits.CSUMStep().matrix([]), solver=solver.LeastSquares_Jac_Solver())
+
 p.run()
