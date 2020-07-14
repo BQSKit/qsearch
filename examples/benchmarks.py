52bddcbe272ab2417b8f134e5dcb8ee0be110dee
Ethan Smith <ethan@ethanhs.me>
2020-07-14 16:54:02 -0700

    Example

--- examples/benchmarks.py
+++ examples/benchmarks.py
@@ -1,11 +1,11 @@
 import qsearch
-from qsearch import unitaries, advanced_unitaries, multistart_solver, parallelizer, solver
+from qsearch import unitaries, advanced_unitaries, leap_compiler, multistart_solver, parallelizer
 import time
 
 # create the project
 with qsearch.Project("benchmarks") as project:
     # add a unitaries to compile
-    project.add_compilation("qft2", unitaries.qft(4))
+    """ project.add_compilation("qft2", unitaries.qft(4))
     project.add_compilation("qft3", unitaries.qft(8))
     project.add_compilation("fredkin", unitaries.fredkin)
     project.add_compilation("toffoli", unitaries.toffoli)
@@ -13,19 +13,22 @@ with qsearch.Project("benchmarks") as project:
     project.add_compilation("or", unitaries.logical_or)
 
     project.add_compilation("miro", advanced_unitaries.mirogate)
-    project.add_compilation("hhl", advanced_unitaries.HHL)
+    project.add_compilation("hhl", advanced_unitaries.HHL) """
 
     # 4 qubit benchmarks (WARNING: These may take days to run.)
-    #project.add_compilation("qft4", unitaries.qft(16))
+    project.add_compilation("qft4", unitaries.qft(16))
     #project.add_compilation("full adder", unitaries.full_adder)
     #project.add_compilation("ethylene", advanced_unitaries.ethylene)
 
 
     # compiler configuration example
     #project["gateset"] = qsearch.gatesets.QubitCNOTRing() # use this to synthesize for the ring topology instead of the default line topology
-    #project["verbosity"] = 2 # use this to have more information reported to stdout and the log files, or set it to 0 to disable logging altogether
-    #project["solver"] = multistart_solver.MultiStart_Solver(8) # Use the more accurate multistart solver
-    #project["parallelizer"] = parallelizer.ProcessPoolParallelizer # Multistart requires nested processes, so we use ProcessPoolExecutor
+    #project["solver"]  = qsearch.solver.BFGS_Jac_SolverNative() # use this to force the compiler to use the Rust version of the BFGS solver instead of using the default setting
+    project["verbosity"] = 2 # use this to have more information reported to stdout and the log files, or set it to 0 to disable logging altogether
+    project["min_depth"] = 6
+    project["compiler_class"] = leap_compiler.LeapCompiler
+    project["solver"] = multistart_solver.MultiStart_Solver(8) # Use the more accurate multistart solver
+    project["parallelizer"] = parallelizer.ProcessPoolParallelizer # Multistart requires nested processes, so we use ProcessPoolExecutor
     # once everything is set up, let the project run!
     project.run()
 
