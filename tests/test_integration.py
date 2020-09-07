b5c931217e53ad5d60373004f673a823ec73f9f5
Ethan Smith <ethan@ethanhs.me>
2020-09-07 15:36:58 -0700

    Fix tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,12 +1,14 @@
 from qsearch import Options, unitaries, backend
 
 def test_per_compilation_options(project):
-    opts = Options(backend=backend.PythonBackend())
     project['backend'] = backend.SmartDefaultBackend()
-    project.add_compilation('qft2', unitaries.qft(4), options=opts)
     project.add_compilation('qft3', unitaries.qft(8))
     project.run()
     qft3_opts = project._compilations['qft3']['options']
     assert 'backend' not in qft3_opts
-    qft2_opts = project._compilations['qft2']['options']
+
+def test_per_compilation_options2(project):
+    opts = Options(backend=backend.PythonBackend())
+    project.add_compilation('qft3', unitaries.qft(8), options=opts)
+    qft2_opts = project._compilations['qft3']['options']
     assert isinstance(qft2_opts.backend, backend.PythonBackend)
--- tests/test_leap.py
+++ tests/test_leap.py
@@ -1,7 +1,7 @@
 
 import qsearch
 import numpy as np
-from qsearch import unitaries, advanced_unitaries, utils, gatesets, leap_compiler, reoptimizing_compiler
+from qsearch import unitaries, advanced_unitaries, utils, gatesets, leap_compiler, reoptimizing_compiler, multistart_solver, parallelizer
 
 def test_leap(project, check_project):
     project.add_compilation("qft4", unitaries.qft(16))
@@ -14,13 +14,8 @@ def test_leap(project, check_project):
 def test_reoptimize(project, check_project):
     target = unitaries.qft(16)
     project.add_compilation("qft4", target)
-    project["min_depth"] = 6
+    project["min_depth"] = 4
     project["compiler_class"] = leap_compiler.LeapCompiler
     project.run()
-    cdict = project.get_result('qft4')
-    best_pair = (cdict["structure"], cdict["vector"])
-    cut_depths=cdict["cut_depths"]
-    project.clear()
-    project.add_compilation('qft4', target, cut_depths=cut_depths, best_pair=best_pair, depth=7)
-    project.run()
+    project.post_process(reoptimizing_compiler.ReoptimizingCompiler(), solver=multistart_solver.MultiStart_Solver(8), parallelizer=parallelizer.ProcessPoolParallelizer, depth=5)
     check_project(project)
