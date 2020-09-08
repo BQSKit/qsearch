7be02e89f907e4c93f264cc023bcab5f412c1999
Ethan Smith <ethan@ethanhs.me>
2020-09-07 18:53:10 -0700

    More test fixes

--- tests/test_gatesets.py
+++ tests/test_gatesets.py
@@ -8,7 +8,7 @@ def test_qubit_cnot_linear(project, check_project):
 
 def test_zxzxzcnotlinear(project, check_project):
     project['gateset'] = gatesets.ZXZXZCNOTLinear()
-    project.add_compilation('qft2', unitaries.qft(4))
+    project.add_compilation('qft3', unitaries.qft(8))
     project.run()
     check_project(project)
 
@@ -20,12 +20,12 @@ def test_qiskitu3linear(project, check_project):
 
 def test_cnotring(project, check_project):
     project['gateset'] = gatesets.QubitCNOTRing()
-    project.add_compilation('qft2', unitaries.qft(4))
+    project.add_compilation('qft3', unitaries.qft(8))
     project.run()
     check_project(project)
 
 def test_adjacency_list(project, check_project):
     project['gateset'] = gatesets.QubitCNOTAdjacencyList([(0,1), (1,2), (2,3), (2,0)])
-    project.add_compilation('qft2', unitaries.qft(4))
+    project.add_compilation('qft3', unitaries.qft(8))
     project.run()
     check_project(project)
