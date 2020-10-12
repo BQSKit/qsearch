42458e3c63e841c0ceb588b86e3d8776d2bf46d8
Marc Davis <marc.davis@berkeley.edu>
2020-10-12 02:04:59 -0400

    fixed the stateprep bug

--- examples/stateprep.py
+++ examples/stateprep.py
@@ -1,4 +1,5 @@
 import qsearch
+from qsearch import leap_compiler
 import numpy as np
 from functools import partial
 
@@ -8,7 +9,8 @@ p = qsearch.Project("stateprep-example")
 
 # configure the project with the stateprep defaults instead of the standard synthesis defaults
 p.configure(**qsearch.defaults.stateprep_defaults)
-p["solver"] = qsearch.solver.LeastSquares_Jac_Solver()
+p["compiler_class"] = leap_compiler.LeapCompiler
+p["solver"] = qsearch.solvers.LeastSquares_Jac_Solver()
 
 # add states that are converted using generate_stateprep_target_matrix
 p.add_compilation("basic_state_test", qsearch.utils.generate_stateprep_target_matrix([0.5,0,0,0.5j,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-0.5j,0,0,0,0,0,0,0,-0.5,0,0,0,0,0]))
