06bc01dd13df7ec3fa37c5abc0049bac9dbd036b
Ethan Smith <ethan@ethanhs.me>
2020-10-13 05:44:09 -0700

    Add docstrings for multistart_solvers

--- qsearch/multistart_solvers.py
+++ qsearch/multistart_solvers.py
@@ -11,21 +11,33 @@ from multiprocessing import Queue, Process
 from .persistent_aposmm import initialize_APOSMM, decide_where_to_start_localopt, update_history_dist, add_to_local_H
 
 def distance_for_x(x, options, circuit):
+    """Calculate the distance between circuit and the target for input x based on the distance metric"""
     if options.inner_solver.distance_metric == "Frobenius":
         return options.error_func(options.target, circuit.matrix(x))
     elif options.inner_solver.distance_metric == "Residuals":
         return np.sum(options.error_residuals(options.target, circuit.matrix(x), np.eye(options.target.shape[0]))**2)
 
 def optimize_worker(circuit, options, q, x0):
+    """Worker function used to run the inner solver in parallel"""
     _, xopt = options.inner_solver.solve_for_unitary(circuit, options, x0)
     q.put((distance_for_x(xopt, options, circuit), xopt))
 
 class MultiStart_Solver(Solver):
+    """A higher accuracy solver based on APOSMM https://www.mcs.anl.gov/~jlarson/APOSMM/
+
+    MultiStart_Solver generally gets better results than other optimizers due to the advanced algorithm
+    to start multiple local optimizers ("inner solvers") and find the global optimum more often.
+    """
 
     def __init__(self, num_threads):
+        """Create a MultiStart_Solver instance. Pass num_threads to set how many threads to use in parallel optimization runs"""
         self.num_threads = num_threads
 
     def solve_for_unitary(self, circuit, options, x0=None):
+        """Optimize the given circuit based on the provided options with initial point x0 (optional).
+        
+        This uses the "inner_solver" options attribute to set which optimizer to use for local optimization runs.
+        """
         if 'inner_solver' not in options:
             options.inner_solver = default_solver(options)
         U = options.target
@@ -80,7 +92,9 @@ class MultiStart_Solver(Solver):
         return (circuit.matrix(xopt), xopt)
 
 class DumbMultiStart_Solver(Solver):
+    """A dumb but effective multi-start solver which tries to cover as much of the optimization space at once"""
     def __init__(self, num_threads):
+        """Create a DumbMultiStart_Solver instance. Pass num_threads to set how many threads to use in parallel optimization runs"""
         self.threads = num_threads if num_threads else 1
 
     def solve_for_unitary(self, circuit, options, x0=None):
