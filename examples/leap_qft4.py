95f9b80454fd4b00bcd4bca4ff33b6a11d8c9e3d
Ethan Smith <ethan@ethanhs.me>
2020-10-14 00:41:32 -0700

    Reduce size of block for LEAP example

--- examples/leap_qft4.py
+++ examples/leap_qft4.py
@@ -7,7 +7,7 @@ with qsearch.Project("leapex") as project:
     # Add 4 qubit qft
     project.add_compilation("qft4", unitaries.qft(16))
     # set a miniumum search depth (to reduce frequent chopping that gets nowhere)
-    project["min_depth"] = 6
+    project["min_depth"] = 4
     # configure qsearch to use the leap compiler
     project["compiler_class"] = leap_compiler.LeapCompiler
     project["verbosity"] = 2
