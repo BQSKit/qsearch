ec1ad21907b8ac354951447185245f4e0ab6da97
Ethan Smith <ethan@ethanhs.me>
2021-01-02 00:37:33 -0800

    Add tests for IBM openqasm assembly

--- tests/test_assemble.py
+++ tests/test_assemble.py
@@ -1,4 +1,4 @@
-from qsearch.assemblers import ASSEMBLER_IBMOPENQASM
+from qsearch.assemblers import ASSEMBLER_IBMOPENQASM, ASSEMBLER_OPENQASM, ASSEMBLER_QISKIT
 from qsearch.gates import *
 from qsearch import utils
 import pytest
@@ -20,7 +20,15 @@ parameters = np.array([5.00000000e-01, 6.68212734e-01, 6.68212734e-01, 2.2584836
 
 cdict = {"structure":circ, "parameters":parameters}
 
-@pytest.mark.skipif(qiskit == None, reason="Qiskit not installed")
+def unitary_from_openqasm(qasm_str):
+    qc = QuantumCircuit.from_qasm_str(qasm_str)
+    backend = qiskit.BasicAer.get_backend('unitary_simulator')
+    job = qiskit.execute(qc, backend)
+    U = job.result().get_unitary()
+    BE = utils.endian_reverse(U)
+    return BE
+
+@pytest.mark.skipif(qiskit is None, reason="Qiskit not installed")
 def test_openqasm_assemble_roundtrip():
     s = ASSEMBLER_IBMOPENQASM.assemble(cdict)
     qc = QuantumCircuit.from_qasm_str(s)
@@ -29,3 +37,23 @@ def test_openqasm_assemble_roundtrip():
     U = job.result().get_unitary()
     BE = utils.endian_reverse(U)
     assert utils.matrix_distance_squared(BE, circ.matrix(parameters)) < 1e-10
+
+ASSEMBLER_GATES = (
+    XGate(),
+    YGate(),
+    ZGate(),
+    U3Gate(),
+    CNOTGate(),
+    CZGate(),
+    XXGate(),
+    ISwapGate(),
+)
+
+@pytest.mark.skipif(qiskit is None, reason="Qiskit not installed")
+@pytest.mark.parametrize("gate", ASSEMBLER_GATES, ids=lambda gate: gate.__class__.__name__)
+def test_assemble_ibmopenqasm(gate):
+    parameters = np.random.uniform(size=(max(gate.num_inputs, 1),))
+    cdict = {"structure": gate, "parameters": parameters}
+    s = ASSEMBLER_IBMOPENQASM.assemble(cdict)
+    U = unitary_from_openqasm(s)
+    assert utils.matrix_distance_squared(U, gate.matrix(parameters)) < 1e-10
